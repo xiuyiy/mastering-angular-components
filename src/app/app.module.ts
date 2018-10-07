e141d6c799069c2917f0e9f0a4a9cbebb85fc814
Ye, Xiuyi <xiye@microstrategy.com>
2018-10-06 21:32:39 -0400

    added enter-task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { TaskListComponent } from './tasks/task-list/task-list.component';
 
 import { TaskComponent } from './tasks/task/task.component';
+import { EnterTaskComponent } from './tasks/enter-task/enter-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
-    TaskComponent
+    TaskComponent,
+    EnterTaskComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tasks/enter-task/enter-task.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'mac-enter-task',
+  templateUrl: './enter-task.component.html',
+  styleUrls: ['./enter-task.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class EnterTaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
