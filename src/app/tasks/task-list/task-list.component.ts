4ad849f6a53ffaa3ebf4a8af1f33bd558811f2b0
Ye, Xiuyi <xiye@microstrategy.com>
2018-10-06 21:03:32 -0400

    1. added task-list 2. added the task-list selector into the app.component.html

--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -7,8 +7,13 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class TaskListComponent implements OnInit {
+  tasks = [
+    {id: 1, title: 'Task 1', done: false},
+    {id: 2, title: 'Task 2', done: true}
+  ];
+  constructor() {
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
